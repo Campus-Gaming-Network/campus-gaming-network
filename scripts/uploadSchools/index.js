65ac3b3b9ac8cc38b0f1449bb0e3e9d6bd44992b
Brandon Sansone <bsansone@users.noreply.github.com>
2020-02-25 20:04:04 -0600

    Prettify

--- scripts/uploadSchools/index.js
+++ scripts/uploadSchools/index.js
@@ -1,6 +1,6 @@
 const firebase = require("firebase");
-const chunk = require('lodash.chunk');
-const kebabCase = require('lodash.kebabcase');
+const chunk = require("lodash.chunk");
+const kebabCase = require("lodash.kebabcase");
 const geohash = require("ngeohash");
 const dotenv = require("dotenv");
 
@@ -9,20 +9,20 @@ const dotenv = require("dotenv");
 const DATA = require("../../data/schools.json");
 
 // Points to local dotenv used in main project
-const result = dotenv.config({ path: '../../.env.local' })
+const result = dotenv.config({ path: "../../.env.local" });
 
 if (result.error) {
   throw result.error;
 }
 
 firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
 
 // Each transaction or batch of writes can write to a maximum of 500 documents.
@@ -33,84 +33,84 @@ const names = {};
 
 // Map the school data fields for use in the db
 const mapSchool = ({
-	NAME,
-	ADDRESS,
-	CITY,
-	COUNTRY,
-	STATE,
-	WEBSITE,
-	ZIP,
-	COUNTY,
-	TELEPHONE,
-	LATITUDE,
-	LONGITUDE
+  NAME,
+  ADDRESS,
+  CITY,
+  COUNTRY,
+  STATE,
+  WEBSITE,
+  ZIP,
+  COUNTY,
+  TELEPHONE,
+  LATITUDE,
+  LONGITUDE
 }) => {
-	const school = {
-		name: NAME,
-		address: ADDRESS,
-		city: CITY,
-		country: COUNTRY,
-		state: STATE,
-		website: WEBSITE,
-		zip: String(ZIP),
-		county: COUNTY,
-		phone: String(TELEPHONE),
-		location: new firebase.firestore.GeoPoint(
-			LATITUDE,
-			LONGITUDE
-        ),
-        geohash: geohash.encode(LATITUDE, LONGITUDE),
-		handle: kebabCase(NAME),
-	};
+  const school = {
+    name: NAME,
+    address: ADDRESS,
+    city: CITY,
+    country: COUNTRY,
+    state: STATE,
+    website: WEBSITE,
+    zip: String(ZIP),
+    county: COUNTY,
+    phone: String(TELEPHONE),
+    location: new firebase.firestore.GeoPoint(LATITUDE, LONGITUDE),
+    geohash: geohash.encode(LATITUDE, LONGITUDE),
+    handle: kebabCase(NAME)
+  };
 
-	if (!names[school.name]) {
-		names[school.name] = 0;
-	} else {
-		names[school.name]++;
-		school.handle = `${school.handle}-${names[school.name]}`;
-    }
+  if (!names[school.name]) {
+    names[school.name] = 0;
+  } else {
+    names[school.name]++;
+    school.handle = `${school.handle}-${names[school.name]}`;
+  }
 
-	return school;
-}
+  return school;
+};
 
 // Wear the magic happens
 const main = async () => {
-	console.log("====================");
-	console.log("Starting...");
-	console.log("====================");
+  console.log("====================");
+  console.log("Starting...");
+  console.log("====================");
 
-	console.log(`${DATA.length} colleges`);
+  console.log(`${DATA.length} colleges`);
 
-	const chunks = chunk(DATA, MAX_DOCUMENTS);
+  const chunks = chunk(DATA, MAX_DOCUMENTS);
 
-	console.log(`${chunks.length} chunks`);
+  console.log(`${chunks.length} chunks`);
 
-	for (const _chunk of chunks) {
-		console.count("Started chunk");
+  for (const _chunk of chunks) {
+    console.count("Started chunk");
 
-		let db = firebase.firestore();
-        let batch = db.batch();
+    let db = firebase.firestore();
+    let batch = db.batch();
 
-		const batches = _chunk.map(mapSchool).map(
-			school => batch.set(db.collection("schools").doc(), school)
-		);
+    const batches = _chunk
+      .map(mapSchool)
+      .map(school => batch.set(db.collection("schools").doc(), school));
 
-		for (const _batch of batches) {
-			_batch.commit().then(() => {
-                console.count("Wrote batch");
-            }).catch((err) => {
-				console.log(`Error on batch commit: ${err}`)
-			});
-		}
+    for (const _batch of batches) {
+      _batch
+        .commit()
+        .then(() => {
+          console.count("Wrote batch");
+        })
+        .catch(err => {
+          console.log(`Error on batch commit: ${err}`);
+        });
+    }
 
-		console.count("Finished chunk");
-	}
+    console.count("Finished chunk");
+  }
 
-	console.log("====================");
-	console.log("Done.");
-	console.log("====================");
+  console.log("====================");
+  console.log("Done.");
+  console.log("====================");
 
-	process.exit(1);
-}
+  process.exit(1);
+};
 
 main();
