f1091aedf28c0c50847ad3ff12d378e09e65c36d
Brandon Sansone <bsansone@users.noreply.github.com>
2020-09-10 17:28:40 -0600

    Add footer

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -3,8 +3,20 @@ import React from "react";
 import { Router } from "@reach/router";
 import { useAuthState } from "react-firebase-hooks/auth";
 import isEmpty from "lodash.isempty";
-import { Box } from "@chakra-ui/core";
-
+import {
+  Box,
+  Link as ChakraLink,
+  List,
+  ListItem,
+  Text,
+  Flex
+} from "@chakra-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHeart,
+  faMugHot,
+  faExternalLinkAlt
+} from "@fortawesome/free-solid-svg-icons";
 // Other
 import { firebaseAuth } from "./firebase";
 import { useAppState, useAppDispatch, ACTION_TYPES } from "./store";
@@ -32,6 +44,8 @@ import UserSilhouette from "./components/UserSilhouette";
 import SchoolSilhouette from "./components/SchoolSilhouette";
 import EventSilhouette from "./components/EventSilhouette";
 import Empty from "./components/Empty";
+import Link from "./components/Link";
+import OutsideLink from "./components/OutsideLink";
 
 const AuthenticatedApp = () => {
   const state = useAppState();
@@ -97,6 +111,116 @@ const AuthenticatedApp = () => {
       {nav}
       <Box as="main" pb={12}>
         {routes}
+      </Box>
+      <Box
+        as="footer"
+        borderTopWidth={1}
+        textAlign="center"
+        pt={{ md: 8, sm: 0 }}
+        pb={8}
+      >
+        <Flex justifyContent="space-between" flexWrap="wrap">
+          <List
+            spacing={2}
+            flexBasis={{ md: "33.3333%", sm: "100%" }}
+            minWidth={{ md: "33.3333%", sm: "100%" }}
+            flexGrow={0}
+            pt={{ md: 0, sm: 8 }}
+          >
+            <ListItem fontSize="xs" fontWeight="bold" textTransform="uppercase">
+              Resources
+            </ListItem>
+            <ListItem fontSize="sm">
+              <Link to="/about-us" color="purple.500" fontWeight={600}>
+                About us
+              </Link>
+            </ListItem>
+            <ListItem fontSize="sm">
+              <Link
+                to="/frequently-asked-questions"
+                color="purple.500"
+                fontWeight={600}
+              >
+                <Text as="abbr" title="Frequently asked questions">
+                  FAQ
+                </Text>
+              </Link>
+            </ListItem>
+            <ListItem fontSize="sm">
+              <ChakraLink
+                href="mailto:support@campusgamingnetwork.com"
+                color="purple.500"
+                fontWeight={600}
+              >
+                Email us
+              </ChakraLink>
+            </ListItem>
+          </List>
+          <List
+            spacing={2}
+            flexBasis={{ md: "33.3333%", sm: "100%" }}
+            minWidth={{ md: "33.3333%", sm: "100%" }}
+            flexGrow={0}
+            pt={{ md: 0, sm: 8 }}
+          >
+            <ListItem fontSize="xs" fontWeight="bold" textTransform="uppercase">
+              Community
+            </ListItem>
+            <ListItem fontSize="sm">
+              Join our{" "}
+              <OutsideLink href="https://discord.gg/dpYU6TY" color="purple.500">
+                Discord
+                <Text as="span" ml={1}>
+                  <FontAwesomeIcon icon={faExternalLinkAlt} size="xs" />
+                </Text>
+              </OutsideLink>
+            </ListItem>
+            <ListItem fontSize="sm">
+              Contribute on{" "}
+              <OutsideLink
+                href="https://github.com/bsansone/campus-gaming-network"
+                color="purple.500"
+                fontWeight={600}
+              >
+                GitHub
+                <Text as="span" ml={1}>
+                  <FontAwesomeIcon icon={faExternalLinkAlt} size="xs" />
+                </Text>
+              </OutsideLink>
+            </ListItem>
+          </List>
+          <List
+            spacing={2}
+            flexBasis={{ md: "33.3333%", sm: "100%" }}
+            minWidth={{ md: "33.3333%", sm: "100%" }}
+            flexGrow={0}
+            pt={{ md: 0, sm: 8 }}
+          >
+            <ListItem fontSize="sm">
+              Made with{" "}
+              <Text d="inline" color="red.500">
+                <FontAwesomeIcon icon={faHeart} />
+              </Text>{" "}
+              and <FontAwesomeIcon icon={faMugHot} /> in{" "}
+              <Text d="inline" fontWeight={600}>
+                Salt Lake City, Utah
+              </Text>
+            </ListItem>
+            <ListItem fontSize="sm">
+              Enjoying the site?{" "}
+              <OutsideLink
+                href="https://www.buymeacoffee.com/cgnbrandon"
+                color="purple.500"
+                fontWeight={600}
+              >
+                Buy me a coffee.
+                <Text as="span" ml={1}>
+                  <FontAwesomeIcon icon={faExternalLinkAlt} size="xs" />
+                </Text>
+              </OutsideLink>
+            </ListItem>
+          </List>
+        </Flex>
       </Box>
     </React.Fragment>
   );
