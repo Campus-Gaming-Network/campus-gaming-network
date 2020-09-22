5733c7870012974079cff4db4959cfe9e14d95d2
Brandon Sansone <bsansone@users.noreply.github.com>
2020-09-22 16:40:40 -0600

    Handle email verification

--- src/pages/auth-action.js
+++ src/pages/auth-action.js
@@ -1,22 +1,17 @@
 import React from "react";
-import { Redirect } from "@reach/router";
-import { useAuthState } from "react-firebase-hooks/auth";
 import queryString from "query-string";
 
 import Empty from "../components/Empty";
 
 import PasswordReset from "./password-reset";
-
-import { firebaseAuth } from "../firebase";
+import VerifyEmail from "./verify-email";
 
 const AuthActionComponents = {
   resetPassword: PasswordReset,
-  recoverEmail: null,
-  verifyEmail: null
+  verifyEmail: VerifyEmail
 };
 
 const AuthAction = props => {
-  const [authenticatedUser, isAuthenticating] = useAuthState(firebaseAuth);
   const { mode, oobCode } = React.useMemo(
     () => queryString.parse(props.location.search),
     [props.location.search]
@@ -26,14 +21,6 @@ const AuthAction = props => {
       mode && !!AuthActionComponents[mode] ? AuthActionComponents[mode] : Empty,
     [mode]
   );
-
-  if (isAuthenticating) {
-    return null;
-  }
-
-  if (!!authenticatedUser) {
-    return <Redirect to="/" noThrow />;
-  }
 
   return <AuthActionComponent {...{ ...props, mode, oobCode }} />;
 };
---
+++ src/pages/verify-email.js
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+  Box,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription
+} from "@chakra-ui/core";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import { firebaseAuth } from "../firebase";
+import { Redirect } from "@reach/router";
+
+const PasswordReset = props => {
+  const [authenticatedUser, isAuthenticating] = useAuthState(firebaseAuth);
+  const [verifyState, setVerifyState] = React.useState("");
+  const [verificationError, setError] = React.useState("");
+
+  const handleVerifyEmail = () => {
+    firebaseAuth
+      .applyActionCode(props.oobCode)
+      .then(() => {
+        firebaseAuth.currentUser
+          .reload()
+          .then(() => {
+            if (firebaseAuth.currentUser.emailVerified) {
+              setVerifyState("success");
+            } else {
+              setVerifyState("error");
+            }
+          })
+          .catch(error => {
+            console.error(error);
+            setError(error.message);
+            setVerifyState("error");
+          });
+      })
+      .catch(error => {
+        console.error(error);
+        setError(error.message);
+        setVerifyState("error");
+      });
+  };
+
+  React.useEffect(() => {
+    if (!isAuthenticating && !!authenticatedUser) {
+      handleVerifyEmail();
+    }
+  }, [isAuthenticating, authenticatedUser]);
+
+  if (isAuthenticating) {
+    return null;
+  }
+
+  if (!authenticatedUser) {
+    return <Redirect to="/not-found" noThrow />;
+  }
+
+  if (!verifyState) {
+    return null;
+  }
+
+  return (
+    <Box as="article" py={16} px={8} mx="auto" fontSize="xl" maxW="3xl">
+      <Alert
+        status={verifyState}
+        variant="subtle"
+        flexDirection="column"
+        justifyContent="center"
+        textAlign="center"
+        height="200px"
+      >
+        <AlertIcon size="40px" mr={0} />
+        <AlertTitle mt={4} mb={1} fontSize="lg">
+          {verifyState === "success"
+            ? "Email verified!"
+            : "Email verification unsuccessful."}
+        </AlertTitle>
+        <AlertDescription maxWidth="sm">
+          {verifyState === "success"
+            ? "Thank you for verifying your email address."
+            : verificationError}
+        </AlertDescription>
+      </Alert>
+    </Box>
+  );
+};
+
+export default PasswordReset;
