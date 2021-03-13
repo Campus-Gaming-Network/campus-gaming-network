c76e03c8795810659af921839e0858bae310ad65
Brandon Sansone <bsansone@users.noreply.github.com>
2021-03-12 19:32:29 -0700

    Mobile vertical padding for article

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,11 +6,11 @@ const Article = ({ children, ...rest }) => {
   return (
     <Box
       as="article"
-      py={16}
+      py={{ base: 6, md: 16 }}
       px={{ base: 0, md: 8 }}
       mx="auto"
       fontSize="xl"
-      maxW={{ base: "100%", md: "3xl" }}
+      maxW={{ base: "100%", sm: "xl", md: "3xl" }}
       {...rest}
     >
       {children}
