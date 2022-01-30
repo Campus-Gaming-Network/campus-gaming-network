918148a7e5b1bee05df4995ec18e815479bd5eb0
Brandon Sansone <bsansone@users.noreply.github.com>
2022-01-30 11:12:26 -0700

    Update firebase admin

--- apps/website/src/firebaseAdmin/index.js
+++ apps/website/src/firebaseAdmin/index.js
@@ -3,9 +3,9 @@ import * as firebaseAdmin from 'firebase-admin';
 try {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
-      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID,
+      private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+      client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+      project_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID,
     }),
     databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID}.firebaseio.com`,
   });
