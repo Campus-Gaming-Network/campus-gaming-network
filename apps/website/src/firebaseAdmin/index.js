660723f2ddfa9efdf2f40065354bb5a4f3d1be32
Brandon Sansone <bsansone@users.noreply.github.com>
2022-01-30 11:24:13 -0700

    testing stuff

--- apps/website/src/firebaseAdmin/index.js
+++ apps/website/src/firebaseAdmin/index.js
@@ -1,12 +1,14 @@
 import * as firebaseAdmin from 'firebase-admin';
 
 try {
+  const cert = {
+    private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    project_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID,
+  };
+  console.log(JSON.stringify(cert));
   firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert({
-      private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
-      client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-      project_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID,
-    }),
+    credential: firebaseAdmin.credential.cert(cert),
     databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID}.firebaseio.com`,
   });
 } catch (error) {
